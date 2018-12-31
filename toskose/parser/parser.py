8ab2ba5381a3cbd5baa23621d67df17d5fdc0ac5
Matteo Bogo <matteo.bogo@gmail.com>
2018-12-31 17:33:24 +0100

    added skeletons for toskose tool and toskose manager

---
+++ toskose/parser/parser.py
@@ -0,0 +1,2 @@
+# TODO business logic for parsing the toskerer declaration file .yml
+# TODO look at TOSCAparser
---
+++ toskose/supervisord/configurator.py
@@ -0,0 +1,2 @@
+# TODO business logic for generating the supervisord.conf
+# look at libraries https://docs.python.org/2/library/configparser.html
