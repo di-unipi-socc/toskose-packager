43a2a440bba2c7abbdae051316f83327cb0ef98f
Matteo Bogo <matteo.bogo@gmail.com>
2019-01-26 14:19:35 +0100

    in toskose-manager updated email service according to app factory pattern

--- toskose-manager/src/api/main/email.py
+++ toskose-manager/src/api/main/email.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from api import mail
 from flask_mail import Message
 from threading import Thread
@@ -11,4 +12,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(app, msg)).start()
+    Thread(
+        target=send_async_email,
+        args=(current_app._get_current_object(), msg)
+    ).start()
