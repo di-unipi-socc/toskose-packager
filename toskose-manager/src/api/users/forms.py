2d161ef4cb933d3ea9f4128ac0378968cdf83a81
Matteo Bogo <matteo.bogo@gmail.com>
2019-01-26 14:13:52 +0100

    in toskose-manager fixed missing User model import in edit_profile form logic, added also a notification when username is successfully updated

--- toskose-manager/src/api/users/forms.py
+++ toskose-manager/src/api/users/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Length
+from api.main.models import User
 
 
 class EditProfileForm(FlaskForm):
