bea641c98ba46cda8bd1c0081b14df700a63730f
matteo <matteo.bogo@gmail.com>
2019-01-30 00:50:24 +0100

    in toskose-manager: added setup.py for api

---
+++ toskose-manager/api/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='toskose-manager-api',
+    version='1.0',
+    description='API for managing TOSCA-based multi-component cloud applications',
+    url='https://github.com/matteobogo/toskose',
+    author='Matteo Bogo',
+    author_email='matteo.bogo@gmail.com',
+    license='MIT',
+    packages=['app'],
+    zip_safe=False
+)
