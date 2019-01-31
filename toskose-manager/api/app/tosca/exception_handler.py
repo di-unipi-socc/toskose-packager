16534ca8768c8579838a5ca3bd08024928d70a2f
Matteo Bogo <matteo.bogo@gmail.com>
2019-01-31 20:44:26 +0100

    in toskose-manager api: added api error handlers for managing the supervisord client exceptions

---
+++ toskose-manager/api/app/tosca/exception_handler.py
@@ -0,0 +1,34 @@
+from app.tosca import api
+
+from app.client.exceptions import SupervisordClientFatalError
+from app.client.exceptions import SupervisordClientConnectionError
+from app.client.exceptions import SupervisordClientProtocolError
+from app.client.exceptions import SupervisordClientFaultError
+
+
+def client_handling_failures(func):
+    """ decorator for handling client failures """
+
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except SupervisordClientConnectionError as ex:
+            print('todo')
+
+    return wrapper
+
+@api.errorhandler(SupervisordClientFatalError)
+def handle_fatal_error(error):
+    return ({ 'message': '{0}'.format(error) }, 500)
+
+@api.errorhandler(SupervisordClientConnectionError)
+def handle_connection_error(error):
+    return ({ 'message': '{0}. Node not found'.format(error) }, 404)
+
+@api.errorhandler(SupervisordClientProtocolError)
+def handle_protocol_error(error):
+    return ({ 'message': '{0}'.format(error) }, 500)
+
+@api.errorhandler(SupervisordClientFaultError)
+def handle_fault_error(error):
+    return ({ 'message': '{0}'.format(error) })
