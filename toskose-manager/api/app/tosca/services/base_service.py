2278dce9ef67b4c72bb2d9e1689adbf41b6fe1ee
Matteo Bogo <matteo.bogo@gmail.com>
2019-02-02 19:47:00 +0100

    in toskose-manager api: added decorator in base service for validating the node identifier, for making a client instance and for validating if the node is reacheable

--- toskose-manager/api/app/tosca/services/base_service.py
+++ toskose-manager/api/app/tosca/services/base_service.py
@@ -1,30 +1,81 @@
-from client.client import ToskoseClientFactory
-from tosca.models import ToskoseNode
+from core.toskose_manager import ToskoseManager
 
+from client.exceptions import SupervisordClientConnectionError
+from client.exceptions import SupervisordClientFatalError
+from client.exceptions import SupervisordClientProtocolError
+from client.exceptions import SupervisordClientFaultError
 
-# TODO
-# will be changed with a "map" generated from an external yml
-# we need id associated to a toskose-unit node
+from tosca.exception_handler import GenericFatalError
+from tosca.exception_handler import ClientFatalError
+from tosca.exception_handler import ClientOperationFailedError
+from tosca.exception_handler import ClientConnectionError
 
-nodes = {
-    '1': ToskoseNode('1','localhost','9001','admin','admin')
-}
-nodes_id = { id: '1' }
-
-# TODO will be moved in the module for handling client
-def get_client_instance(id):
-    """ return the client instance """
-    node = nodes[id]
-    return ToskoseClientFactory.create(
-        type='XMLRPC',  # TODO dynamic with configuration
-        host=node.host,
-        port=node.port,
-        username=node.username,
-        password=node.password
-    )
 
 class BaseService():
 
-    def __init__(self, node_id):
-        self._node_id = node_id
-        self.client = get_client_instance(node_id)
+    """ client decorators
+
+    init_client() instantiates the client and check node\'s reachability.
+    If the validate_connection argument is set to False, then it suppresses the
+    SupervisordConnectionError, so it will not be catched by the api errorhandler,
+    but a boolean istance attribute regarding the reachability (is_reacheable)
+    is always stored.
+    This strategy meets the requirements to give partial info about the nodes
+    (e.g. get_node_info() in NodeService) without raise an exception to the api
+    controller, even if the node is offline.
+    If the validate_connection argument is set to True, then the client is
+    triggered before the function calling, and a SupervisordConnectionError
+    will be raised if the node is offline.
+
+    """
+
+    @classmethod
+    def init_client(cls, validate_node=False, validate_connection=False):
+        def decorator(func):
+            def wrapper(self, *args, **kwargs):
+
+                """ validate the node identifier """
+                if validate_node:
+                    node_id = kwargs.get('node_id')
+                    if not ToskoseManager.get_instance().get_node_data(
+                        node_id=node_id):
+                        raise ResourceNotFoundError(
+                            'node {0} not found'.format(node_id))
+
+                """ get the client instance """
+                node_id = kwargs.get('node_id')
+                self._client = \
+                    ToskoseManager.get_instance().get_node_client_instance(node_id)
+
+                """ check the reachability of the node """
+                self._is_reacheable = True
+                try:
+                    self._client.get_identification()
+                except SupervisordClientConnectionError as conn_err:
+                    self._is_reacheable = False
+
+                """ client connection validation """
+                if validate_connection:
+                    if not self._is_reacheable:
+                        node_id = kwargs.get('node_id')
+
+                        raise ClientConnectionError(
+                            'node {0} is offline'.format(node_id))
+
+                try:
+                    res = func(self, *args, **kwargs)
+                except (SupervisordClientFaultError, SupervisordClientProtocolError):
+                    raise ClientOperationFailedError('operation failed')
+                except SupervisordClientFatalError:
+                    raise ClientFatalError('an error is occurred during \
+                        the communication with the node')
+                except:
+                    raise GenericFatalError('an unexpected error is occurred')
+
+                return res
+
+            return wrapper
+        return decorator
+
+    def __init__(self):
+        pass
