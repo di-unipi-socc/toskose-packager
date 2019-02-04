5a08a0d4189bfb2e6ab22f8449e479e001ba9eb1
Matteo Bogo <matteo.bogo@gmail.com>
2019-02-04 04:21:40 +0100

    in toskose-manager api: updated loggingfacility: fixed imports, fixed get_instance() name, fixed bug in the __init__() (no self assignment in the __instance var), updated logger formatter (removed pathname and lineno), fixed bug during the creation of a default path for logging (when LOGS_PATH env is missing), added check for path creation

--- toskose-manager/api/app/core/logging.py
+++ toskose-manager/api/app/core/logging.py
@@ -4,7 +4,7 @@ import time
 import datetime
 import logging
 
-from config import AppConfig
+from app.config import AppConfig
 
 
 class LoggingFacility:
@@ -13,7 +13,7 @@ class LoggingFacility:
     __instance = None
 
     @staticmethod
-    def getInstance():
+    def get_instance():
         """ The static access method """
 
         if LoggingFacility.__instance == None:
@@ -24,27 +24,31 @@ class LoggingFacility:
 
         if LoggingFacility.__instance != None:
             raise Exception('This is a singleton')
+        else:
+            LoggingFacility.__instance = self
 
         self._logger = logging.getLogger(__name__)
         self._logger.setLevel(logging.DEBUG)
 
         """ Formatter """
-        self.formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s \
-            [in %(pathname)s:%(lineno)d]')
+        self.formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+            # [in %(pathname)s:%(lineno)d]')
 
         """ Output path """
         logs_path = AppConfig._LOGS_PATH
         if not logs_path:
             """ logs path not set -- use default """
-            self.__create_log_path()
+            logs_path = self.__create_default_log_path()
+
+        if not os.path.exists(logs_path):
+            sys.exit('failed to setup logging')
 
         now = datetime.datetime.now()
-        dirname = 'logs/'
-        log_path = os.path.join(dirname, ('log_' + now.strftime("%Y-%m-%d") + '.log'))
+        logs_path = os.path.join(logs_path, ('log_' + now.strftime("%Y-%m-%d") + '.log'))
 
         """ File Handler """
         self._file_handler = logging.handlers.RotatingFileHandler(
-            log_path,
+            logs_path,
             maxBytes=10240,
             backupCount=10)
 
@@ -57,11 +61,14 @@ class LoggingFacility:
         self._logger.addHandler(self._file_handler)
         self._logger.addHandler(streamHandler)
 
-    def __create_log_path(self):
+    def __create_default_log_path(self):
 
         base_dir = Path(__file__).parent.parent.parent
-        if not os.path.exists(os.path.join(base_dir, 'logs')):
-            os.mkdir('logs/')
+        logs_path = os.path.join(base_dir.resolve(), 'logs/')
+        if not os.path.exists(logs_path):
+            os.mkdir(logs_path)
+
+        return logs_path
 
     def get_logger(self):
         return self._logger
