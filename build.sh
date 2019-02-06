4e53b5ddfa2364c14b5dac107638cf73a4228f2a
matteo <matteo.bogo@gmail.com>
2019-02-07 00:51:00 +0100

    updated build script:  added params for versioning and component building choice, added also automatic tagging of the latest version and pushing to the dockerhub

--- build.sh
+++ build.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 
-echo "build toskose-unit image"
-docker build \
--t diunipisocc/toskose-unit-base:1.0.0 \
---build-arg APP_VERSION=1.0.0 \
---build-arg PYTHON_VERSION=2.7.15 \
---build-arg SUPERVISORD_VERSION=3.3.5 \
---build-arg DEBIAN_VERSION=stretch \
--f toskose-unit/Dockerfile toskose-unit/.
+type=$1
+version=$2
 
-echo "build toskose-manager image"
-docker build \
--t diunipisocc/toskose-manager:1.0.0 \
---build-arg APP_VERSION=1.0.0 \
--f toskose-manager/api/Dockerfile-prod toskose-manager/api/.
+if [ -z "${type}" ]; then
+    echo "missing operation type (unit | manager)"
+    exit 1
+fi
+
+if [ -z "${version}" ]; then
+    echo "missing version"
+    exit 1
+fi
+
+if [ "${type}" == 'unit' ]; then
+
+    echo "build toskose-unit image (version: ${type}).."
+    docker build \
+    -t diunipisocc/toskose-unit:${version} \
+    --build-arg APP_VERSION=${version} \
+    --build-arg PYTHON_VERSION=2.7.15 \
+    --build-arg SUPERVISORD_VERSION=3.3.5 \
+    --build-arg DEBIAN_VERSION=stretch \
+    -f toskose-unit/Dockerfile toskose-unit/.
+
+elif [ "${type}" == 'manager' ]; then
+
+    echo "build toskose-manager image (version: ${version}).."
+    docker build \
+    -t diunipisocc/toskose-manager:${version} \
+    --build-arg PYTHON_VERSION=3.7.2 \
+    --build-arg APP_VERSION=${version} \
+    -f toskose-manager/api/Dockerfile toskose-manager/api/.
+    
+else
+    echo "operation type not recognized"
+    exit 1
+fi
+
+echo "generating the latest version.."
+docker tag diunipisocc/toskose-${type}:${version} diunipisocc/toskose-${type}:latest
+
+echo "pushing on the repository.."
+docker push diunipisocc/toskose-${type}:${version}
+docker push diunipisocc/toskose-${type}:latest
