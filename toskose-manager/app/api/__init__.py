97059df1df8ef72fc798e566c92e7267019a0411
Matteo Bogo <matteo.bogo@gmail.com>
2019-02-07 16:05:42 +0100

    updated controller's namespaces imports

--- toskose-manager/app/api/__init__.py
+++ toskose-manager/app/api/__init__.py
@@ -16,8 +16,8 @@ api = Api(
 # TODO
 
 
-from app.tosca.controllers.node_controller import ns as ns_node
+from app.api.controllers.node_controller import ns as ns_node
 api.add_namespace(ns_node, path='/node')
 
-from app.tosca.controllers.subprocess_controller import ns as ns_subprocess
+from app.api.controllers.subprocess_controller import ns as ns_subprocess
 api.add_namespace(ns_subprocess, path='/subprocess')
