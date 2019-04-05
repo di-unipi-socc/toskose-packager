b380082a1d6f4467b376a8abee4294b133be3124
Matteo Bogo <matteo.bogo@gmail.com>
2019-04-05 11:55:10 +0200

    fixed package name typo: toskose-tool -> toskosetool
    Former-commit-id: 1860d59b085dc7b39bd6c388de5ef31a3e00f770

---
+++ toskosetool/toskose.py
@@ -0,0 +1,10 @@
+from toskosetool.gui.interactive_menu import InteractiveMenu
+from toskosetool.config import AppConfig
+
+def main():
+    
+    gui = InteractiveMenu()
+
+
+if __name__ == "__main__":
+    main()
