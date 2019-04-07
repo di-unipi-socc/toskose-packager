41a68e6a476c04cbfe3264b5714364a6265683b6
Matteo Bogo <matteo.bogo@gmail.com>
2019-04-07 02:03:52 +0200

    toskose-tool: update cli effects for printing text (added attributes like bolds, blink, etc..)
    Former-commit-id: 0aee0c6273217af5cc449f6d65d21e6679af7d7e

--- toskose-tool/app/gui/effects.py
+++ toskose-tool/app/gui/effects.py
@@ -3,9 +3,9 @@ from termcolor import colored
 from pyfiglet import figlet_format
 
 
-def print_cli(text, color, font='slant', figlet=False):
+def print_cli(text, color, attrs=[], font='slant', figlet=False):
     if colored:
         if not figlet:
-            six.print_(colored(text, color))
+            six.print_(colored(text, color, attrs=attrs))
         else:
-            six.print_(colored(figlet_format(text, font=font), color))
+            six.print_(colored(figlet_format(text, font=font), color, attrs=attrs))
--- toskose-tool/app/toskose.py
+++ toskose-tool/app/toskose.py
@@ -1,8 +1,8 @@
-from app.gui.interactive_menu import cli
+from app.gui.interactive_menu import CLI
 
 def main():
     
-    cli()
+    CLI().run()
 
 
 if __name__ == "__main__":
