98ab36d4a0cf72d33c7efea02c64c177d66712df
Matteo Bogo <matteo.bogo@gmail.com>
2019-04-12 17:29:18 +0200

    in toskose-tool added setuptools configuration
    Former-commit-id: 53478864077a62f14a82cf19717e4e0e2671f3ef

---
+++ toskose-tool/app/__init__.py
@@ -0,0 +1,2 @@
+__name__ = 'Toskose'
+__version__ = '0.1.0'
---
+++ toskose-tool/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import app
+
+
+setup(
+    name=app.__name__,
+    version=app.__version__,
+    packages=find_namespace_packages(),
+
+    scripts=[],
+
+    install_requires=[],
+
+    package_data={},
+
+    # metadata (PyPI)
+    author="Me",
+    author_email="matteo.bogo@protonmail.com",
+    description="A tool for translating a TOSCA-based configuration into docker-compose.",
+    license="GPL"
+    keywords="toskose docker docker-compose tosca",
+    url="https://github.com/matteobogo/toskose",
+
+)
