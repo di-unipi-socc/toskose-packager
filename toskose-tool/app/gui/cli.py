750f73a4f828a20af05bdfb9f91f9e3f2aec94c7
Matteo Bogo <matteo.bogo@gmail.com>
2019-04-13 22:19:39 +0200

    In toskose-tool, updated CLI:  added alerts types enum, now current session is managed using dataclasses (also including print functions),  added notification area, fixed bug in action validator, updated error handling with notification,  added logic for validating and building the tosca model during the loading of the csar archive, print_app_info moved within the class
    Former-commit-id: 6729c53ad3a3fd289c630bfc7d2c411d9a1ff240

--- toskose-tool/app/gui/cli.py
+++ toskose-tool/app/gui/cli.py
@@ -1,10 +1,12 @@
 import os
 import sys
 from enum import Enum, auto
+from dataclasses import dataclass, field
 import app
 from app.gui.effects import print_cli
 from app.config import AppConfig
-from app.tosca.parser import ToscaLoader
+from app.tosca.loader import ToscaLoader
+from app.tosca.validator import ToscaValidator
 from app.tosca.model import ToscaModel
 from app.common.exception import ToscaParsingError
 from app.common.exception import ToscaValidationError
@@ -12,17 +14,6 @@ from app.common.logging import LoggingFacility
 
 
 logger = LoggingFacility.get_instance().get_logger()
-
-
-current_session_text = \
-"""
-=== Current Session ===
-Archive: {0}
-Manifest: {1}
-Validated: {2}
-Toskosed: {3}
-=======================
-"""
 
 
 class Menus(Enum):
@@ -60,51 +51,66 @@ menus_options = {
     }
 }
 
+class Alerts(Enum):
+    Success = auto()
+    Warning = auto()
+    Failure = auto()
 
-class CLI():
+class CLI():  
+
+    class BaseSession():
+
+        def print(self):
+            return [attr for attr in dir(self) if not attr.startswith('__') and not attr == 'print']
+
+    @dataclass
+    class SessionPaths(BaseSession):
+        archive: str = None
+        csar_dir: str = None
+        manifest: str = None
+
+        def print(self):
+            return '\n'.join(['- {0}: {1}'.format(attr, 'Not loaded yet' if not getattr(self, attr) else (getattr(self, attr))) for attr in super().print()])
+
+    @dataclass
+    class SessionFlags(BaseSession):
+        validated: bool = False
+        topology: bool = False
+        toskosed: bool = False
+
+        def print(self):
+            return '\n'.join(['- {0}: {1}'.format(attr, 'No' if not getattr(self, attr) else 'Yes') for attr in super().print()])
 
     def __init__(self):
 
-        self.archive_path = None
-        self.manifest_path = None
-        self.is_valid = False
-        self.is_toskosed = False
+        self._paths = CLI.SessionPaths()
+        self._flags = CLI.SessionFlags()
 
-        self.csar_metadata = None
-        self.tosca_model = None
+        self._csar_metadata = None
+        self._tosca_model = None
 
         self.menu_stack = [Menus.Main]
         self.current_action = None
-
-        self.notification = None
+        self.notify = None
 
 
     def run(self):
     
-        self.menu()
-
-    def menu(self):
-        """ """
-
         while True:
 
             self.load_menu()
+            
             self.__action_validator(input('\nMake your choice: '))
             if self.current_action is not None:
                 self.__action_handler()
 
     def load_menu(self):
-        """ """
 
         os.system('clear')
-        print_app_info()
-
+        self.print_app_info()
         self.print_session()
         self.print_menu()
-
-        if self.notification is not None:
-            print_cli('\n{0}'.format(self.notification), color='red')
-            self.notification = None
+        self.print_notification()
 
     def __action_validator(self, input):
         """ Validate actions inserted by the user """
@@ -113,14 +119,14 @@ class CLI():
 
             action_idx = int(input)
 
-            if action_idx < 0 and action_idx > len(menus_options[self.menu_stack[-1]]['options'])-1:
+            if action_idx < 0 or action_idx > len(menus_options[self.menu_stack[-1]]['options'])-1:
                 raise ValueError
             else: 
                 self.current_action = action_idx
                 
         except ValueError as err:
             self.current_action = None
-            self.notification = 'It doesn \'t seem a valid option. Try Again!'
+            self.notify = ('It doesn\'t seem a valid option. Try Again!', Alerts.Failure)
     
     def __action_handler(self):
         """ Call a subroutine by name based on the current menu and the current selected action 
@@ -129,7 +135,11 @@ class CLI():
         obtained using list[-1]
         """
 
-        getattr(self, menus_options[self.menu_stack[-1]]['subroutines'][self.current_action])()
+        try:
+            getattr(self, menus_options[self.menu_stack[-1]]['subroutines'][self.current_action])()
+        except Exception as err:
+            logger.exception(err)
+            self.notify = ('An internal error is occurred', Alerts.Failure)
     
     def go_back_menu(self):
         """ Go to the previous menu following the path on the menu stack """
@@ -147,35 +157,44 @@ class CLI():
         self.menu_stack.append(Menus.Loader) 
 
     def tosca_load_from(self, source='File'):
-        """ """
+        """ Load a CSAR archive from a source """
 
-        """ user input """
         while True:
 
             archive_path = None
             if source == 'File':
                 archive_path = input('\nInsert the file path (0 Abort): ')
             
+            # Abort
             if archive_path == '0':
                 break
 
             try:
-                
+
+                # Loading
                 tl = ToscaLoader(archive_path)
-                self.archive_path, self.yaml_path, self.csar_metadata = \
-                tl.archive_path, tl.yaml_path, tl.csar_metadata
+                self._paths = CLI.SessionPaths(**tl.get_paths())
+
+                # Validation
+                ToscaValidator(os.path.join(self._paths.csar_dir, self._paths.manifest)).validate()
+                self._flags.validated = True
+                self._csar_metadata = tl.csar_metadata
+
+                # Build Model
+                self._tosca_model = ToscaModel(self._paths.manifest)
+                self._flags.topology = True
+
                 break
             
             except (ToscaParsingError, ToscaValidationError) as err:
-                print_cli('{0}'.format(str(err)), color="red") 
-
-        """ build model """
-        self.tosca_model = ToscaModel(self.archive_path)
-
+                self.notify = (err, Alerts.Failure)
+            except Exception as err:
+                logger.exception(err)
+                self.notify = ('An internal error is occurred', Alerts.Failure)
+             
         self.go_main_menu() 
 
     def tosca_metadata(self):
-        """ """
         pass
 
     def toskoserization(self):
@@ -186,8 +205,19 @@ class CLI():
 
         print_cli('Goodbye!', color='blue', attrs=['bold'])
         sys.exit()
-        
 
+    def print_app_info(self):
+        """ Show info about the app """
+
+        print_cli(app.__name__, color="blue", attrs=['bold'], figlet=True)
+        print_cli("version {0}\n".format(app.__version__), color="yellow")
+
+    def print_session(self):
+        """ Show the current session """
+
+        print_cli('=== Current Session ===\n{0}\n{1}\n'
+            .format(self._paths.print(), self._flags.print()), color="green", attrs=['bold']) 
+    
     def print_menu(self):
         """ Show the current menu options """
 
@@ -198,22 +228,22 @@ class CLI():
         back_option = menus_options[self.menu_stack[-1]]['options'][0] # self.menu_stack[-1] is the current menu
         menu_text += '\n0. {0}'.format(back_option)
 
-        print_cli('Menu\n\n{0}'.format(menu_text), color='blue', attrs=['bold'])
+        print_cli('Menu\n\n{0}\n'.format(menu_text), color='blue', attrs=['bold']) 
 
-
-    def print_session(self):
+    def print_notification(self):
         """ """
 
-        print_cli(current_session_text.format( \
-            'not loaded yet' if self.archive_path is None else self.archive_path,
-            'not loaded yet' if self.manifest_path is None else self.manifest_path
-            'No' if not self.is_valid else 'Yes',
-            'No' if not self.is_toskosed else 'Yes'
-        ), color="green", attrs=['bold'])
+        def print_alert(msg, type, attrs=['bold']):
 
+            color = 'blue'
+            if type == Alerts.Success:
+                color = 'green'
+            elif type == Alerts.Warning:
+                color = 'yellow'
+            elif type == Alerts.Failure:
+                color = 'red'
+            print_cli(msg, color=color, attrs=attrs)
 
-def print_app_info():
-    """ Show info about the app """
-
-    print_cli(app.__name__, color="blue", attrs=['bold'], figlet=True)
-    print_cli("version {0}\n".format(app.__version__), color="yellow")
+        if self.notify is not None:
+            print_alert(self.notify[0], self.notify[1])
+            self.notify = None
