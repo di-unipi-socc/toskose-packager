e69c0dcb77441c983206350c3a97b28d35f465d2
Matteo Bogo <matteo.bogo@gmail.com>
2019-04-17 07:57:52 +0200

    updated tests for thinking-app
    Former-commit-id: 943fd7b67ae4e5f60c693aaef23e2fbd53536aab

--- tests/thinking/build-test-without-args.sh
+++ tests/thinking/build-test-without-args.sh
@@ -2,7 +2,7 @@
 
 docker build -t stephenreed/jenkins-java8-maven-git-toskosed:latest \
 --build-arg TOSKOSE_UNIT_IMAGE=diunipisocc/toskose-unit:latest \
---build-arg TOSCA_NODES_CONTAINER_IMAGE=stephenreed/jenkins-java8-maven-git:latest \
+--build-arg TOSCA_SRC_IMAGE=stephenreed/jenkins-java8-maven-git:latest \
 --build-arg TOSCA_NODES_SOFTWARE_APPS_NAMES=api \
 --build-arg TOSCA_NODES_SOFTWARE_STUFF_PATH=./node-1/ \
 -f Dockerfile .
