116ef3f66ab542206a71a17f4d93ff165b9ec38b
Matteo Bogo <matteo.bogo@gmail.com>
2019-05-27 20:50:53 +0200

    Removed Config, Updated constants - added docker (toskosed) image required data - added default supervisord envs - added toskose-manager required data (envs) - added a default output path
    Former-commit-id: 005dbc8920434845a23448f428c1934993708fc8

--- toskose-tool/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class AppConfig(object):
-    """ Application Configuration """
-
-    _APP_REPOSITORY = 'https://github.com/matteobogo/toskose'
-    _TOSKOSE_UNIT_IMAGE = 'diunipisocc/toskose-unit'
-    _TOSKOSE_UNIT_IMAGE_TAG = 'latest'
-    _TOSKOSE_MANAGER_IMAGE = 'diunipisocc/toskose-manager:latest'
--- toskose-tool/app/constants.py
+++ toskose-tool/app/constants.py
@@ -1,4 +1,41 @@
 APP_REPOSITORY = 'https://github.com/matteobogo/toskose'
+
+# DOCKER
 TOSKOSE_UNIT_IMAGE = 'diunipisocc/toskose-unit'
-TOSKOSE_UNIT_IMAGE_TAG = 'latest'
-TOSKOSE_MANAGER_IMAGE = 'diunipisocc/toskose-manager:latest'
+TOSKOSE_UNIT_TAG = 'latest'
+TOSKOSE_MANAGER_IMAGE = 'diunipisocc/toskose-manager'
+TOSKOSE_MANAGER_TAG = 'latest'
+
+DEFAULT_TOSKOSE_MANAGER_NAME = 'toskose-manager'
+
+DOCKER_IMAGE_REQUIRED_DATA = [
+    'repository',
+    'user',
+    'password',
+    'name',
+    'tag'
+]
+
+# SUPERVISORD
+SUPERVISORD_HTTP_PORT = 9000
+
+DEFAULT_SUPERVISORD_HTTP_USER = 'admin'
+DEFAULT_SUPERVISORD_HTTP_PASSWORD = 'admin'
+DEFAULT_SUPERVISORD_LOG_LEVEL = 'info'
+
+SUPERVISORD_ENVS = {
+    'http_port': None,
+    'http_user': DEFAULT_SUPERVISORD_HTTP_USER,
+    'http_password': DEFAULT_SUPERVISORD_HTTP_PASSWORD,
+    'log_level': DEFAULT_SUPERVISORD_LOG_LEVEL
+}
+
+# MANAGER
+TOSKOSE_MANAGER_REQUIRED_DATA = {
+    'http_port': 10000,
+    'http_user': 'admin',
+    'http_password': 'admin',
+}
+
+# EXTRA
+DEFAULT_OUTPUT_PATH = 'toskose_out'
