be40af83607fab96776edcbb782d552c74fa0cef
Matteo Bogo <matteo.bogo@gmail.com>
2019-05-27 23:48:55 +0200

    moved toskose-unit subdir to another repository: https://github.com/matteobogo/toskose-unit
    Former-commit-id: 59930e6ca4e1c70d6ca5b7bad9b4a15b08d6d9ef

--- toskose-unit/base/scripts/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/toskose/supervisord/bundle/supervisord \
--c /toskose/supervisord/config/supervisord.conf
--- toskose-unit/base/scripts/test/test-1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-while [ 0 ]; do
-  echo "Hi! I am alive!"
-  sleep 5
-done
