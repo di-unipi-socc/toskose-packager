b4a919d438bc006ec4c6c02b076215e1acfb8237
Matteo Bogo <matteo.bogo@gmail.com>
2019-06-02 23:28:21 +0200

    Updated setup.py - Removed requirements.txt, now dependencies are handled by the setup.py - added description/author/email/license using dunders from init -

--- setup.py
+++ setup.py
@@ -1,24 +1,43 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 import app
+
+
+test_require = [
+    'pytest',
+    'mock'
+]
+
+install_requires = [
+    'tosca-parser==1.4.0',
+    'termcolor==1.1.0',
+    'pyfiglet==0.8.post1',
+    'six==1.12.0',
+    'colorama==0.4.1',
+    'setuptools==41.0.0',
+    'click==7.0',
+    'docker==3.7.2',
+    'ruamel.yaml==0.15.94',
+    'jsonschema==3.0.1',
+]
 
 
 setup(
     name=app.__name__,
     version=app.__version__,
-    packages=find_namespace_packages(),
-
-    scripts=[],
-
-    install_requires=[],
-
-    package_data={},
-
-    # metadata (PyPI)
-    author="Me",
-    author_email="matteo.bogo@protonmail.com",
-    description="A tool for translating a TOSCA-based configuration into docker-compose.",
-    license="GPL",
+    description=app.__doc__.strip(),
+    long_description="",
     keywords="toskose docker docker-compose tosca",
     url="https://github.com/matteobogo/toskose",
-
+    author=app.__author__,
+    author_email=app.__email__,
+    license=app.__license__,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'toskose = app.__main__:main',
+        ],
+    },
+    #extras_require=[],
+    install_requires=install_requires,
+    tests_require=test_require,
 )
