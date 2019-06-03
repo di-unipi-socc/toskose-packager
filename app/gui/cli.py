87d62c6d969081c4b265e5b6f88a36c6c55bf9a0
Matteo Bogo <matteo.bogo@gmail.com>
2019-06-03 23:18:37 +0200

    Updated CLI - Removed subcommands - Removed validation - Removed handling failure decorator Only toskose process can be triggered - added option to enable/disable Docker registry push of "toskosed" images - added toskose config path arg

--- app/gui/cli.py
+++ app/gui/cli.py
@@ -1,55 +1,25 @@
 import click
-
-# from . import __name__
-# from . import __version__
-
-#from app.toskose import Toskose
 from app.toskose import Toskoserizator
-from app.common.commons import Alerts
-from app.gui.effects import print_notification_cli
 
 
-def handling_failure(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except Exception as err:
-            print_notification_cli(err, Alerts.Failure)
-    return wrapper
-
-@click.group()
-#@click.version_option(version=__version__)
-@click.option('--quiet', '-q', is_flag=True, help='Give less output.')
-@click.option('--debug', is_flag=True, help='Enable debug mode.')
-@click.pass_context
-def cli(ctx, quiet, debug):
-    """
-    Description Soon
-    """
-
-    ctx.obj = Toskoserizator(debug=debug, quiet=quiet)
-
-@cli.command(name='validate')
-@click.pass_context
-@click.argument('file', type=click.Path(exists=True))
-@handling_failure
-def validate(ctx, file):
-    """ 
-    Soon
-    """
-
-    print_notification_cli('Not implemented yet', Alerts.Warning)
-
-    # validated = ctx.obj.validate(file=file)
-    
-    # print_notification_cli('\u2713 Validated.', Alerts.Success) if validated \
-    #         else print_notification_cli('\u274C Not Validated.', Alerts.Failure)
-    
-@cli.command(name='toskoserize')
-@click.pass_context
+@click.command()
 @click.argument(
     'csar_path', 
     type=click.Path(exists=True),
+)
+@click.argument(
+    'config_path',
+    type=click.Path(exists=True),
+    required=False
+)
+@click.option(
+    '--output-path', '-o', 
+    help='The path for the output.'
+)
+@click.option(
+    '--enable-push', '-p',
+    help='Enable/Disable pushing of Docker images.',
+    show_default=True
 )
 @click.option(
     '--docker-url', 
@@ -57,24 +27,21 @@ def validate(ctx, file):
     default='unix:///var/run/docker.sock',
     show_default=True
 )
-@click.option(
-    '--output-path', '-o', 
-    help='The path for the output.'
-)
-@handling_failure
-def toskose(ctx, csar_path, output_path, docker_url):
+@click.option('--quiet', '-q', is_flag=True, help='Give less output.')
+@click.option('--debug', is_flag=True, help='Enable debug mode.')
+def cli(csar_path, config_path, output_path, enable_push, docker_url, quiet, debug):
     """
-    Description Soon
+    A tool for translating a TOSCA application into docker-compose.
     """
 
-    ctx.obj.csar_path = csar_path
-    if output_path:
-        ctx.obj.output_path = output_path
+    tsk = Toskoserizator(debug=debug, quiet=quiet)
+    
     if docker_url:
-        ctx.obj.docker_url = docker_url
+        tsk.docker_url = docker_url
 
-    ctx.obj.toskosed()
-
-
-if __name__ == '__main__':
-    cli()
+    tsk.toskosed(
+        csar_path,
+        config_path=config_path,
+        output_path=output_path,
+        enable_push=enable_push
+    )
