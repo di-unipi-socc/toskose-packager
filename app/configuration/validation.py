713fd8ccc9dd4b7535f43871ce5591231ba63a56
Matteo Bogo <matteo.bogo@gmail.com>
2019-06-03 22:41:27 +0200

    Updated config schema and its validation - removed the 'api' field from nodes and manager section, api's fields are in the first level, together with the docker subfield. - fixed the json schema (removed nulls for tag and registry_password), removed api section. - Updated schema validation: removed nodes check against the tosca model (already done during the auto-completation or config default generation routine) - removed the partialvalidation error: only validationerror if the config cannot be auto-completed

--- app/configuration/validation.py
+++ app/configuration/validation.py
@@ -32,49 +32,30 @@ class ConfigValidator:
                 raise FatalError('The toskose configuration schema is corrupted. Validation cannot be done.')
             except Exception as err:
                 logger.exception(err)
-                raise FatalError(CommonErrorMessages._DEFAULT_FATAL_ERROR_MSG)
+                raise FatalError(CommonErrorMessages._DEFAULT_FATAL_ERROR_MSG)   
 
-    @staticmethod
-    def _validate_data(config, tosca_model):
-        """ Validate data in the Toskose configuration against the tosca model nodes. """
 
-        invalid = False
-
-        # nodes
+    def _validate_nodes(config, tosca_model):
         for container in tosca_model.containers:
+            
             if container.name not in config['nodes']:
-                logger.warn('Missing [{}] node data from the configuration file'.format(
-                    container.name
-                ))
-                invalid = True
+                raise ValidationError('Missing node [{}] in the configuration.'.format(
+                    container.name))
+            
+            if 'docker' not in config['nodes'][container.name]:
+                raise ValidationError('Missing docker section in the node [{}]'.format(
+                    container.name))
 
-        # manager
-        if 'manager' not in config:
-            logger.warn('Missing manager data from the configuration file')
-            invalid = True
-
-        return invalid
-
-    def _handle_error(self, error):
-        if 'is a required property' in error.message:
-            extracted = re.findall(r"\'(.*?)\'", error.message)
-            raise PartialValidationError('{} field is missing'.format(
-                extracted[0] if extracted else 'unknown'
-            ))
-        else:
-            raise ValidationError(error.message)
-        
 
     def validate_config(self, config, tosca_model=None):
         """ Validate a Toskose configuration file """
 
         try:
             jsonschema.validate(instance=config, schema=self._config_schema)
-            if tosca_model is not None:
-                if ConfigValidator._validate_data(config, tosca_model):
-                    raise PartialValidationError('Missing data from the configuration file')
+            # if tosca_model is not None:
+            #     ConfigValidator._validate_nodes(config, tosca_model)
         except jsonschema.exceptions.ValidationError as err:
-            self._handle_error(err)
+            raise ValidationError(err.message)
         except jsonschema.exceptions.SchemaError as err:
             logger.error(err)
             raise FatalError('The toskose configuration schema is corrupted. Validation cannot be done.')
