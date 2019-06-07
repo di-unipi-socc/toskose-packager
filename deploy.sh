f7f4488dc547d6ba21b9e93e4e9ec007a3943c93
Matteo Bogo <matteo.bogo@gmail.com>
2019-06-08 00:27:45 +0200

    Updated travis with deploy script - added script for pushing all the thinking toskosed image to the docker hub (needed integration tests with toskose manager)

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MAVEN_IMAGE="giulen/thinking-maven-toskosed"
+NODE_IMAGE="giulen/thinking-node-toskosed"
+MONGODB_IMAGE="giulen/thinking-mongodb-toskosed"
+MANAGER_IMAGE="giulen/thinking-manager"
+
+images=( $MAVEN_IMAGE $NODE_IMAGE $MONGODB_IMAGE $MANAGER_IMAGE )
+
+docker --version
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+for image in "${images[@]}"
+do
+    docker push $image
+done
